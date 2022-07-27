97efea022b0dc056190a25be58871cc1a7285cf9
mPalmaRodriguez <mpalma14@misena.edu.co>
2022-07-27 10:21:01 -0500

    version v3

--- src/Components/Navbar/Index.js
+++ src/Components/Navbar/Index.js
@@ -1,6 +1,5 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import logoNav from '../Assets/img/navlogo.webp';
 import './navbar.css';
 
